d064c1f6c7227c4343c7bd895b2f28523e0da9c0
cvilla <villac@mit.edu>
2024-11-11 15:19:43 -0500

    tdm module done I think and tested

--- sv_folder/hdl/tdm_receive.sv
+++ sv_folder/hdl/tdm_receive.sv
@@ -1,30 +1,105 @@
 `timescale 1ns / 1ps
 `default_nettype none
 
-module tdm_receive 
-    #(  parameter BIT_WIDTH = 24 
+
+module tdm_receive
+    #(  parameter BIT_WIDTH = 24,
         parameter SLOTS = 4
      )
      (  input wire sck, // Serial Clock
         input wire ws, // Word Select
         input wire sd, // Serial Data
-        input rst_in, // system reset
-        output [BIT_WIDTH-1:0] audio_out; // audio from microphone
+        input wire rst_in, // system reset
+        output logic wso, // slot number for audio
+        output logic [BIT_WIDTH-1:0] audio_out, // audio from microphone
         output logic audio_valid_out // valid signal, HIGH when valid
-     ) 
-    
+     );
+   
     localparam [5:0] TOTAL_CYCLES = 31; // cycle # that indicates looping back
-    logic sck_counter; 
-    logic [$ceil($log2(SLOTS))-1:0] curr_slot; // can also just harcode this since we know our slot #
-    logic starting; // flag to know to stay on slot 0 for start
+    logic [5:0] sck_counter;
+    logic [2:0] curr_slot; // can also just harcode this since we know our slot #s
+    logic active; // flag to know to stay on slot 0 for start
+
 
     always_ff @(posedge sck)begin
+        if(rst_in)begin
+            sck_counter<=0;
+            curr_slot <=0;
+           
+       
+        end
+        else begin
+            if(ws)begin
+                sck_counter <=0;
+                curr_slot<=0;
+                audio_out<=0;
+                audio_valid_out<=0;
+                active<=1;
+            end
+            else begin
+
+
+                if(active) begin
+                    // add logic so it only starts doing reads when a WS signal comes by
+                    if(sck_counter <BIT_WIDTH-1) begin
+                        // shift in bits, MSB first
+                        audio_out <= {audio_out[BIT_WIDTH-2:0],sd};
+                        sck_counter <= sck_counter + 1;
+
+
+                    end
+                    else if(sck_counter == BIT_WIDTH-1)begin
+                        audio_out <= {audio_out[BIT_WIDTH-2:0],sd};
+                        // raise high for a cycle
+                        audio_valid_out<=1;
+
+
+                        sck_counter <= sck_counter + 1;
+
+
+                    end
+                    else if(sck_counter == TOTAL_CYCLES )begin
+                        // set up like the beginning
+                        sck_counter <=0;
+                        audio_out<=0;
+                        audio_valid_out<=0;
+                        if(curr_slot == SLOTS -1)begin
+                            curr_slot <=0;
+                            active<=0;
+                        end
+                        else begin
+                            curr_slot<=curr_slot +1;
+                        end
+                       
+
+
+                    end
+                    else begin
+                        audio_valid_out <=0;
+                        sck_counter <= sck_counter + 1;
+                    end
+
+
+
+
+                end
+            end
+
+
+        end
+
 
     end
 
 
 
+
+
+
 endmodule // tdm_receive
+
+
+
 
 
 
---
+++ sv_folder/sim/test_tdm_receive.py
@@ -0,0 +1,174 @@
+import cocotb
+import os
+import random
+import sys
+from math import log
+import logging
+from pathlib import Path
+from cocotb.clock import Clock
+from cocotb.triggers import Timer, ClockCycles, RisingEdge, FallingEdge, ReadOnly,with_timeout
+from cocotb.utils import get_sim_time as gst
+from cocotb.runner import get_runner
+
+
+
+
+
+
+@cocotb.test()
+async def test_single_microphone(dut):
+    """ Test if data from a single microphone is received correctly. """
+
+
+    dut._log.info("Starting...")
+    cocotb.start_soon(Clock(dut.sck, 10, units="ns").start())
+   
+
+
+    # Generate 24 bits of random data for a single microphone
+    sample_data = [1,0,1,0,  0,0,1,0,  0,0,0,1,   1,0,0,0,   0,1,0,0,   1,1,0,0  ]
+    expected_value = 0b101000100001100001001100 #int(''.join(map(str, sample_data)), 2)
+
+
+    # Send the WS pulse
+    dut.ws.value = 1
+    await RisingEdge(dut.sck)
+    await FallingEdge(dut.sck)
+    dut.ws.value = 0
+
+
+    # Send the sample data bit-by-bit on the rising edge of SCK
+    for bit in sample_data:
+        dut.sd.value= bit
+        await RisingEdge(dut.sck)
+
+
+    # Check if the received data matches the expected value
+    await RisingEdge(dut.sck)  # Allow time for `audio_valid` signal to propagate
+    assert dut.audio_valid_out.value == 1, "audio_valid should be high after receiving 24 bits"
+    assert dut.audio_out.value == expected_value, f"Expected {expected_value}, but got {dut.audio_out.value}"
+
+
+    for i in range(9):
+        await RisingEdge(dut.sck)
+    assert dut.curr_slot.value == 1, "curr slot should now be 1"
+   
+
+
+@cocotb.test()
+async def test_four_microphones(dut):
+    """ Test if data from four microphones is received correctly. """
+
+
+   
+    dut._log.info("Starting...")
+    cocotb.start_soon(Clock(dut.sck, 10, units="ns").start())
+   
+
+
+    # Generate 24 bits of random data for a single microphone
+    sample_data = [1,0,1,0,  0,0,1,0,  0,0,0,1,   1,0,0,0,   0,1,0,0,   1,1,0,0  ]
+    expected_value = 0b101000100001100001001100 #int(''.join(map(str, sample_data)), 2)
+
+
+    # Send the WS pulse
+    dut.ws.value = 1
+    await RisingEdge(dut.sck)
+    await FallingEdge(dut.sck)
+    dut.ws.value = 0
+
+
+    for i in range(4):
+        #assert dut.curr_slot.value == (i)%4, f"curr slot should now be {(i%4)}, but got {dut.curr_slot.value}"
+        # Send the sample data bit-by-bit on the rising edge of SCK
+        for bit in sample_data:
+            dut.sd.value= bit
+            await RisingEdge(dut.sck)
+
+
+        # Check if the received data matches the expected value
+        await RisingEdge(dut.sck)  # Allow time for `audio_valid` signal to propagate
+        assert dut.audio_valid_out.value == 1, f"audio_valid should be high after receiving 24 bits, failed on {i} signal"
+        assert dut.audio_out.value == expected_value, f"Expected {expected_value}, but got {dut.audio_out.value}"
+
+
+        for j in range(7):
+            await RisingEdge(dut.sck)
+       
+
+
+@cocotb.test()
+async def test_two_samples_four_microphones(dut):
+    """ Test if two consecutive samples from four microphones are received correctly. """
+
+
+    dut._log.info("Starting...")
+    cocotb.start_soon(Clock(dut.sck, 10, units="ns").start())
+   
+
+
+    # Generate 24 bits of random data for a single microphone
+    sample_data = [1,0,1,0,  0,0,1,0,  0,0,0,1,   1,0,0,0,   0,1,0,0,   1,1,0,0  ]
+    expected_value = 0b101000100001100001001100 #int(''.join(map(str, sample_data)), 2)
+
+
+    for _ in range(2):
+        # Send the WS pulse
+        dut.ws.value = 1
+        await RisingEdge(dut.sck)
+        await FallingEdge(dut.sck)
+        dut.ws.value = 0
+
+
+        for i in range(4):
+            #assert dut.curr_slot.value == (i)%4, f"curr slot should now be {(i%4)}, but got {dut.curr_slot.value}"
+            # Send the sample data bit-by-bit on the rising edge of SCK
+            for bit in sample_data:
+                dut.sd.value= bit
+                await RisingEdge(dut.sck)
+
+
+            # Check if the received data matches the expected value
+            await RisingEdge(dut.sck)  # Allow time for `audio_valid` signal to propagate
+            assert dut.audio_valid_out.value == 1, f"audio_valid should be high after receiving 24 bits, failed on {i} signal"
+            assert dut.audio_out.value == expected_value, f"Expected {expected_value}, but got {dut.audio_out.value}"
+
+
+            for j in range(7):
+                await RisingEdge(dut.sck)
+   
+
+
+
+
+def tdm_receive_runner():
+    """Simulate the tdm receiver using the Python runner."""
+    hdl_toplevel_lang = os.getenv("HDL_TOPLEVEL_LANG", "verilog")
+    sim = os.getenv("SIM", "icarus")
+    proj_path = Path(__file__).resolve().parent.parent
+    sys.path.append(str(proj_path / "sim" / "model"))
+    sources = [proj_path / "hdl" / "tdm_receive.sv"]
+    build_test_args = ["-Wall"]
+    parameters = {'BIT_WIDTH': 24, 'SLOTS':4} #!!!change these to do different versions
+    sys.path.append(str(proj_path / "sim"))
+    runner = get_runner(sim)
+    runner.build(
+        sources=sources,
+        hdl_toplevel="tdm_receive",
+        always=True,
+        build_args=build_test_args,
+        parameters=parameters,
+        timescale = ('1ns','1ps'),
+        waves=True
+    )
+    run_test_args = []
+    runner.test(
+        hdl_toplevel="tdm_receive",
+        test_module="test_tdm_receive",
+        test_args=run_test_args,
+        waves=True
+    )
+
+
+if __name__ == "__main__":
+    tdm_receive_runner()
